2c3f527b1effbde910d2964471edd0766d310692
Kyle Williams <me@kylewilliams.co.uk>
2021-02-13 15:41:42 +0800

    Finish translation widget

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import Convert from './Convert';
 import Dropdown from './Dropdown';
 
 const options = [
@@ -25,13 +26,16 @@ const Translate = () => {
       <div className="ui form">
         <div className="field">
           <label>Enter text</label>
-          <input value="text" onChange={(e) => setText(e.target.value)} />
+          <input value={text} onChange={(e) => setText(e.target.value)} />
         </div>
       </div>
       <Dropdown label="Select a language"
                 options={options}
                 selected={language}
                 onSelectedChange={setLanguage} />
+      <hr />
+      <h3 className="ui header">Translated text</h3>
+      <Convert language={language} text={text} />
     </Fragment>
   );
 };
