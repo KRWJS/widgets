bcfc75327b274fefba766080f30755ae1746381e
Kyle Williams <me@kylewilliams.co.uk>
2021-07-25 14:38:31 +0800

    Remove accordion icon

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -13,7 +13,6 @@ const Accordion = ({ items }) => {
     return (
       <Fragment key={item.title}>
         <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
-          <i className="dropdown icon"></i>
           {item.title}
         </div>
         <div className={`content ${active}`}>
